02f3c11a7f09d7542f70a24bed3193b7e4a0278e
travis-ci <travis@travis-ci.org>
2016-01-23 16:17:54 +0000

    [ci skip] publish gh-pages

---
+++ module/page/index/index_d078579.js
@@ -0,0 +1,1 @@
+define("page/index",function(n,e){e.init=function(n){new Vue({el:n,data:function(){return{content:"写些你想说的话,\n用换行排列小人!",scale:1,color:"#FC7D63"}},replace:!1,template:'<div class="p-index">\n    <h1>性感积木</h1>\n</div>\n',components:{}})}});
