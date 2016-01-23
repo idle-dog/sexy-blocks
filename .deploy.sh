34c14d4a1ebed0070cdcbced93b87ec512626b2b
fouber <fouber.NET@gmail.com>
2016-01-24 00:10:01 +0800

    update .travis.yml

--- .deploy.sh
+++ .deploy.sh
@@ -4,7 +4,7 @@
 fis release -r ./src -Documpd ./output
 
 if [ -d output ]; then
-    # 提交代码至gh-pages分支
+    # 提交代码至gh-pages
     echo "➥ Commit files"
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${REPO_SLUG}.git ${REPO_SLUG} > /dev/null
     rm -rf ${REPO_SLUG}/*
